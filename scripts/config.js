7c86b2e1d65034db2d35e0f170b3682d14e7b4db
KinjiroVN-NguyenMinhHieu <hieubkk62@gmail.com>
2026-02-01 11:38:57 +0700

    update apikey free https://www.themoviedb.org/settings/api

--- scripts/config.js
+++ scripts/config.js
@@ -6,7 +6,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { auth } from "./firebase.js";
 
-export const TMDB_API_KEY = "9b7c3ede447b14c5e0e9d33a137ddac9";
+export const TMDB_API_KEY = "51ce916d19fbeb3d8481ea109a7109be";
 
 addEventListener("scroll", () => {
   if (window.scrollY === 0) {
